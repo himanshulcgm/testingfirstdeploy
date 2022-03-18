315a5ad519e5939357e8b4e822d461f27fd83ff4
himanshulcgm <92711959+himanshulcgm@users.noreply.github.com>
2022-03-18 22:37:16 +0530

    now checking (#2)
    Co-authored-by: himanshus@lcgmtech.com <himashus@lcgmtech.com>

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,7 +4,7 @@ import Styles from './header.module.css'
 function Header() {
   return (
     <div className={`${Styles.navigation} container`}>
-        <div>new logo Logo</div>
+        <div>Checking for new branch</div>
         <div className={Styles.navBox}>
             <div className={Styles.nav_items}>Home</div>
             <div className={Styles.nav_items}>About</div>
