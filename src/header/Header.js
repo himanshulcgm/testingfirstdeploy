dc4dc4971d7206c26ef43965d38597810632b873
himanshus@lcgmtech.com <himashus@lcgmtech.com>
2022-03-18 22:23:24 +0530

    header edit

---
+++ src/header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Styles from './header.module.css'
+
+function Header() {
+  return (
+    <div className={`${Styles.navigation} container`}>
+        <div>new logo Logo</div>
+        <div className={Styles.navBox}>
+            <div className={Styles.nav_items}>Home</div>
+            <div className={Styles.nav_items}>About</div>
+            <div className={Styles.nav_items}>Portfolio</div>
+            <div className={Styles.nav_items}>Resume</div>
+            <div className={Styles.nav_items}>Contact</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
